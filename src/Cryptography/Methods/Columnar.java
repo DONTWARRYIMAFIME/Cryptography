48c5fc5bba94178ef1a885ab2991e6db009ab978
Kostyukovich_V_A <demon4ik228228@gmail.com>
2021-03-13 17:19:30 +0300

    Columnar method decryption

--- src/Cryptography/Methods/Columnar.java
+++ src/Cryptography/Methods/Columnar.java
@@ -1,90 +1,138 @@
 package Cryptography.Methods;
 
-import Cryptography.RegularMath;
-
-import java.util.Arrays;
-import java.util.Objects;
-
 public class Columnar implements CryptographyMethod {
 
     @Override
     public String encrypt(String sourceStr, String key) {
-        String[][] mat = generateMatrix(sourceStr, key);
+        char[][] mat = generateEncryptionMatrix(sourceStr, key);
+        System.out.println("Normal matrix: ");
         printMatrix(mat);
-        return convertMatrix(mat);
+        sortMatrix(mat);
+        System.out.println("Sorted matrix: ");
+        printMatrix(mat);
+        return convertEncryptionMatrix(mat);
     }
 
     @Override
     public String decrypt(String sourceStr, String key) {
-        return "null";
+        char[][] mat = generateDecryptionMatrix(sourceStr, key);
+        printMatrix(mat);
+        return convertDecryptionMatrix(mat);
     }
 
-    private void printMatrix(String[][] mat) {
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[i].length; j++) {
-                System.out.print(mat[i][j] != null ? String.format("%4s", mat[i][j]) : "  ");
+    private void printMatrix(char[][] mat) {
+        for (char[] chars : mat) {
+            for (char aChar : chars) {
+                System.out.print(aChar + " ");
             }
             System.out.println();
         }
     }
 
     //Encryption
-    private String[][] generateMatrix(String sourceStr, String key) {
-        int keyLen = key.length();
+    private char[][] generateEncryptionMatrix(String sourceStr, String key) {
 
-        int rows = (sourceStr.length() - 1) / keyLen + 3;
-        int cols = keyLen;
+        int cols = key.length();
+        int rows = (sourceStr.length() - 1) / cols + 2;
 
-        String[][] mat = new String[rows][cols];
+        char[][] mat = new char[rows][cols];
 
         //Fill first row with key
-        for (int i = 0; i < cols; i++) {
-            mat[0][i] = Character.toString(key.charAt(i));
-        }
-
-        String[] sorted = key.split("");
-        Arrays.sort(sorted);
-
-        //Calculate read order
-        for (int i = 0; i < sorted.length; i++) {
-            int id = getId(mat, sorted[i]);
-            mat[1][id] = Integer.toString(i + 1);
-        }
+        System.arraycopy(key.toCharArray(), 0, mat[0], 0, cols);
 
         //Fill matrix with source
         for (int i = 0; i < sourceStr.length(); i++) {
-            int r = i / keyLen + 2;
-            int c = i % keyLen;
+            int r = i / cols + 1;
+            int c = i % cols;
 
-            mat[r][c] = Character.toString(sourceStr.charAt(i));
+            mat[r][c] = sourceStr.charAt(i);
         }
 
         return mat;
     }
 
-    private int getId(String[][] mat, String value) {
-        for (int i = 0; i < mat[0].length; i++) {
-            if (Objects.equals(value, mat[0][i]) && mat[1][i] == null) {
-                return i;
-            }
-        }
-
-        return 0;
-    }
-
-    private String convertMatrix(String[][] mat) {
+    private String convertEncryptionMatrix(char[][] mat) {
         StringBuilder str = new StringBuilder();
 
         for (int i = 0; i < mat[0].length; i++) {
-            int index = RegularMath.linearSearch(mat[1], Integer.toString(i + 1));
-            for (int j = 2; j < mat.length; j++) {
-                String symbol = mat[j][index];
+            for (int j = 1; j < mat.length; j++) {
+                str.append(mat[j][i]);
+            }
+        }
 
-                if (symbol != null) {
-                    str.append(symbol);
+        return str.toString();
+    }
+
+    private void sortMatrix(char[][] mat) {
+
+        for (int i = 0; i < mat[0].length - 1; i++)
+        {
+            for (int j = (mat[0].length - 1); j > i; j--)
+            {
+                if (mat[0][j - 1] > mat[0][j])
+                {
+                    for (int k = 0; k < mat.length; k++) {
+                        char temp = mat[k][j - 1];
+                        mat[k][j - 1] = mat[k][j];
+                        mat[k][j] = temp;
+                    }
                 }
             }
+        }
+    }
 
+    //Decryption
+    private char[][] generateDecryptionMatrix(String sourceStr, String key) {
+
+        int cols = key.length();
+        int rows = (sourceStr.length() - 1) / cols + 2;
+
+        char[][] mat = new char[rows][cols];
+
+        //Fill first row with key
+        System.arraycopy(key.toCharArray(), 0, mat[0], 0, cols);
+
+        //Fill matrix with source
+        int fullBlocks = sourceStr.length() % cols;
+        int iterator = 0;
+
+        for (int i = 0; i < cols; i++) {
+            int col = getMinId(mat);
+
+            int blockLen = col < fullBlocks ? rows : rows - 1;
+
+            for (int j = 1; j < blockLen; j++) {
+                mat[j][col] = sourceStr.charAt(iterator);
+                iterator++;
+            }
+        }
+
+        return mat;
+    }
+
+    private int getMinId(char[][] mat) {
+        int minId = -1;
+        int minVal = 0;
+
+        for (int i = 0; i < mat[0].length; i++) {
+            if (mat[1][i] == '\u0000') {
+                if (minId == -1 || mat[0][i] < minVal ) {
+                    minId = i;
+                    minVal = mat[0][minId];
+                }
+            }
+        }
+
+        return minId;
+    }
+
+    private String convertDecryptionMatrix(char[][] mat) {
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 1; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                str.append(mat[i][j]);
+            }
         }
 
         return str.toString();
